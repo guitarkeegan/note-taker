b778903a1e4cd83d26fa04993c867b266c52b587
guitarkeegan <keegananglim@gmail.com>
2022-06-24 11:48:57 -0700

    add proc

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-const { json } = require("express");
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const { v4: uuidv4 } = require('uuid');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 
